a10f885ab95f827340c77334ac47db76a5e3bb63
mike castleman <m@mlcastle.net>
2024-12-01 00:07:10 -0500

    day 01

--- day01.ts
+++ day01.ts
@@ -1,7 +1,19 @@
-import { readSplit, show } from './util.ts';
+import { Pair, readSplit, show, sumBy, zipWith } from "./util.ts";
+import { MultiSet } from "mnemonist";
 
-const parse = (x: string) => '';
+const parse = (x: string) => x.split(/ +/).map(Number) as Pair<number>;
 
-const input = (await readSplit(1, '\n', false)).map(parse);
+const input = (await readSplit(1, "\n", false)).map(parse);
 
-await show('');
+const l1 = input.map(([a]) => a).sort();
+const l2 = input.map(([, b]) => b).sort();
+
+const score = sumBy(zipWith(l1, l2, (a, b) => Math.abs(a - b)));
+
+await show(score);
+
+const s2 = MultiSet.from(l2);
+
+const score2 = sumBy(l1, (n) => n * s2.count(n));
+
+await show(score2);
